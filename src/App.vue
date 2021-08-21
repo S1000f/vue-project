c40e551931217548edf35abc47c483a528e84d97
Nick <dohp.kim@gmail.com>
2021-08-21 23:06:13 +0900

    router-link

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/databinding">Data Binding</router-link> |
-    <router-link to="/rendering">Renderings</router-link> |
-    <router-link to="/template-directive">Template directive</router-link> |
-    <router-link to="/event">Event</router-link> |
-    <router-link to="/computed">Computed And Watch</router-link> |
-    <router-link to="/apidatabinding">API data binding</router-link> |
-    <router-link to="/nestedcomponent">Components</router-link> |
-    <router-link to="/slot">Slot</router-link> |
-    <router-link to="/provider">Provider / Inject</router-link> |
-    <router-link to="/calc">Calculator1</router-link> |
-    <router-link to="/compoCalc">Composition Calculator</router-link> |
-    <router-link to="/store">Vuex</router-link> |
-    <router-link to="/login">Login</router-link> |
-    <router-link to="/transition">Transition</router-link>
+    <div>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/databinding">Data Binding</router-link> |
+      <router-link to="/rendering">Renderings</router-link> |
+      <router-link to="/template-directive">Template directive</router-link> |
+      <router-link to="/event">Event</router-link> |
+      <router-link to="/computed">Computed And Watch</router-link> |
+      <router-link to="/apidatabinding">API data binding</router-link> |
+      <router-link to="/nestedcomponent">Components</router-link> |
+    </div>
+    <div>
+      <router-link to="/slot">Slot</router-link> |
+      <router-link to="/provider">Provider / Inject</router-link> |
+      <router-link to="/calc">Calculator1</router-link> |
+      <router-link to="/compoCalc">Composition Calculator</router-link> |
+      <router-link to="/store">Vuex</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/transition" tag="button">Transition</router-link> |
+      <router-link to="/route">Router</router-link>
+    </div>
   </div>
   <router-view/>
 </template>
